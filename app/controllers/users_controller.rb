7833d9ca34dffd9aad336f43beba42418a371e14
gon0821 <121909372+gon0821@users.noreply.github.com>
2024-08-13 13:54:12 +0000

    finished 10-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :corrent_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -20,11 +23,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -38,5 +39,19 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  # ログイン済みユーザーかどうかを確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインを先に済ませてください！"
+      redirect_to login_url, status: :see_other
+    end
+  end
+
+  def corrent_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
   end
 end
